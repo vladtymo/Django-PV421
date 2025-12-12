6dc52bb5615bb82dfeaf9d5a5540748e0b5c11f7
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-12-12 18:29:28 +0200

    Create home page

--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('barbers/', include('barbers.urls'))
 ]
---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+import home.views
+
+urlpatterns = [
+    path('', home.views.home, name='home'),
+]
