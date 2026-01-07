f448732763c712a1ab49465d8be2a7e6c1c51c0f
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2026-01-07 20:25:00 +0200

    Add barber admin model

--- barbers/admin.py
+++ barbers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from barbers.models import Barber
+
+class BarberAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position', 'experience']
+    search_fields = ['name', 'position']
+    list_filter = ['position']
+
 # Register your models here.
+admin.site.register(Barber, BarberAdmin)
